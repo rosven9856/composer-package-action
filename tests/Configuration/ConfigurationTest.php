c1cb92e1207ecbd612459273a3a6253aeda91e0a
Your Name <rosven9856@gmail.com>
2024-08-14 17:26:23 +0300

    fixed tests and added tests for other application paths.

--- tests/Configuration/ConfigurationTest.php
+++ tests/Configuration/ConfigurationTest.php
@@ -9,9 +9,18 @@ use PHPUnit\Framework\TestCase;
  */
 class ConfigurationTest extends TestCase
 {
+    const string DEFAULT_ENV_GITHUB_WORKSPACE = '/usr/bin/app';
+    const string OTHER_ENV_GITHUB_WORKSPACE = '/app';
+    const string OTHER_ENV_BUILD_DIRECTORY_NAME = '.build_directory';
+    const string OTHER_ENV_BUILD_FILE_NAME = 'build_file.zip';
+
+    protected Configuration $configuration;
+
     protected function setUp(): void
     {
+        putenv('GITHUB_WORKSPACE=' . self::DEFAULT_ENV_GITHUB_WORKSPACE);
 
+        $this->configuration = new Configuration();
     }
 
     /**
@@ -20,9 +29,10 @@ class ConfigurationTest extends TestCase
      */
     public function testCheckDefaultOptionRoot()
     {
-        $configuration = new Configuration();
-
-        $this->assertEquals($configuration->get('root'), '/usr/bin/app');
+        $this->assertEquals(
+            $this->configuration->get('root'),
+            self::DEFAULT_ENV_GITHUB_WORKSPACE
+        );
     }
 
     /**
@@ -31,9 +41,10 @@ class ConfigurationTest extends TestCase
      */
     public function testCheckDefaultOptionBuildDirectory()
     {
-        $configuration = new Configuration();
-
-        $this->assertEquals($configuration->get('build.directory'), '/usr/bin/app/.build');
+        $this->assertEquals(
+            $this->configuration->get('build.directory'),
+            self::DEFAULT_ENV_GITHUB_WORKSPACE . '/.build'
+        );
     }
 
     /**
@@ -42,8 +53,61 @@ class ConfigurationTest extends TestCase
      */
     public function testCheckDefaultOptionBuildFile()
     {
+        $this->assertEquals(
+            $this->configuration->get('build.file'),
+            self::DEFAULT_ENV_GITHUB_WORKSPACE . '/.build/package.zip'
+        );
+    }
+
+    /**
+     * @covers Configuration::get
+     * @return void
+     */
+    public function testCheckOtherOptionRoot()
+    {
+        putenv('GITHUB_WORKSPACE=' . self::OTHER_ENV_GITHUB_WORKSPACE);
+
         $configuration = new Configuration();
 
-        $this->assertEquals($configuration->get('build.file'), '/usr/bin/app/.build/package.zip');
+        $this->assertEquals(
+            $configuration->get('root'),
+            self::OTHER_ENV_GITHUB_WORKSPACE
+        );
+    }
+
+    /**
+     * @covers Configuration::get
+     * @return void
+     */
+    public function testCheckOtherOptionBuildDirectory()
+    {
+        putenv('GITHUB_WORKSPACE=' . self::OTHER_ENV_GITHUB_WORKSPACE);
+        putenv('BUILD_DIRECTORY_NAME=' . self::OTHER_ENV_BUILD_DIRECTORY_NAME);
+
+        $configuration = new Configuration();
+
+        $this->assertEquals(
+            $configuration->get('build.directory'),
+            self::OTHER_ENV_GITHUB_WORKSPACE . DIRECTORY_SEPARATOR . self::OTHER_ENV_BUILD_DIRECTORY_NAME
+        );
+    }
+
+    /**
+     * @covers Configuration::get
+     * @return void
+     */
+    public function testCheckOtherOptionBuildFile()
+    {
+        putenv('GITHUB_WORKSPACE=' . self::OTHER_ENV_GITHUB_WORKSPACE);
+        putenv('BUILD_DIRECTORY_NAME=' . self::OTHER_ENV_BUILD_DIRECTORY_NAME);
+        putenv('BUILD_FILE_NAME=' . self::OTHER_ENV_BUILD_FILE_NAME);
+
+        $configuration = new Configuration();
+
+        $this->assertEquals(
+            $configuration->get('build.file'),
+            self::OTHER_ENV_GITHUB_WORKSPACE . DIRECTORY_SEPARATOR . self::OTHER_ENV_BUILD_DIRECTORY_NAME .
+            DIRECTORY_SEPARATOR . self::OTHER_ENV_BUILD_FILE_NAME
+        );
     }
 }
